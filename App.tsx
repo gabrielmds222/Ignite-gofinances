d828e502db06d2fa26e42d6549759120f524894b
Gabriel <gabrielmedsilva@outlook.com>
2022-01-25 20:30:08 -0300

    create react native with expo bare workflow

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Welcome from './src/components/Welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Welcome title="App com expo bare workflow e typescript"/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/Welcome.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+interface Props {
+    title: string;
+}
+
+export function Welcome({title}:Props) {
+  return (
+  <View>
+      <Text>{title}</Text>
+  </View>
+  );
+}
+
+export default Welcome;
