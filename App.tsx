edaf2c9011c0af70080c4cc917a312050b0d41a1
Gabriel <gabrielmedsilva@outlook.com>
2022-01-26 10:59:17 -0300

    adiciona styled-components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dashboard } from './src/screens/dashboard/index';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> Primeira aplicação </Text>
-      <StatusBar style="auto" />
-    </View>
+    <Dashboard/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { Text } from 'react-native';
+import { Container, Title } from "./styles";
+
+export function Dashboard() {
+    return(
+        <Container>
+            <Title> Dashboard </Title>
+        </Container>
+    );
+}
