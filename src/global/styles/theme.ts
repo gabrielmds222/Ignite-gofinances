8dbdbb423c7bb1c461e485374d97ba05619fcaa2
Gabriel <gabrielmedsilva@outlook.com>
2022-01-26 21:49:17 -0300

    adiciona biblioteca vector icons

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -9,11 +9,17 @@ export default {
         succes_light: 'rgba(18, 164, 84, 0.5)',
 
         attetion: '#E83F5B',
-        attetion_light: 'rgba(232. 63, 91, 0.5)',
+        attetion_light: 'rgba(232, 63, 91, 0.5)',
 
-        shape: 'FFFFFF',
+        shape: '#FFFFFF',
         title: '#363F5F',
-        text: '969CB2',
+        text: '#969CB2',
         background: '#F0F2F5',
+    },
+
+    fonts: {
+        regular: 'Poppins_400Regular',
+        medium: 'Poppins_500Medium',
+        bold: 'Poppins_700Bold',
     }
 }
