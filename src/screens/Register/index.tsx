fa9bb2a6e4f83969cd2b83fc1265fbd39fb66295
Gabriel <gabrielmedsilva@outlook.com>
2022-01-27 14:18:10 -0300

    Componente de Input

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Input } from '../../components/Form/Input/index';
+import { 
+    Container,
+    Header,
+    Title,
+    Form
+} from './styles';
+
+export function Register() {
+    return(
+        <Container>
+            <Header>
+                <Title> Cadastro </Title>
+            </Header>
+
+            <Form>
+                <Input 
+                    placeholder="Nome"
+                />
+
+                <Input 
+                    placeholder="Preço"
+                />
+            </Form>
+        </Container>
+    );
+}
