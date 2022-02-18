b37c02a48a53d09a5353652300efa6473b1108c0
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-02-18 01:33:54 -0300

    renderizando botões de login na tela

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components/native';
-import { RFValue } from 'react-native-responsive-fontsize';
+import { RFValue, RFPercentage } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
     flex: 1;
@@ -46,3 +46,10 @@ export const Footer = styled.View`
 
     background-color: ${({theme}) => theme.colors.secondary};
 `; 
+
+export const FooterWrapper = styled.View`
+    margin-top: ${RFPercentage(-4)}px;
+    padding: 0 32px;
+
+    justify-content: space-between;
+`;
