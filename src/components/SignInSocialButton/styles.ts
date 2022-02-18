eac978afa44a6d30e2433b476dbf2d7b139fd0e7
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-02-18 01:27:49 -0300

    componente dos botões de login

---
+++ src/components/SignInSocialButton/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+import { RectButton} from 'react-native-gesture-handler';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const Button = styled(RectButton)`
+
+`;
+
+export const ImageContainer = styled.View`
+
+`;
+
+export const Text = styled.Text`
+
+`;
