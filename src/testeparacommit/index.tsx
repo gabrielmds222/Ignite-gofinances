d36bfd28819c96e396bbfd33f8a69dacbb775e3d
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-02-18 02:46:18 -0300

    testando commit da branch

---
+++ src/testeparacommit/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+interface Props {
+    
+}
+ 
+const : FunctionComponent<Props> = () => {
+    return (  );
+}
+ 
+export default ;
