9842ba00cd8e2cc7a6f75186b226bcba54736ed6
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-02-22 01:39:51 -0300

    adiciona credenciais do google

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, ReactNode, useContext } from 'react';
-
+// import { Alert } from 'react-native';
+import * as AuthSession from 'expo-auth-session';
 
 interface AuthProviderProps {
     children: ReactNode;
@@ -13,6 +14,7 @@ interface User {
 }
 interface IAuthContextData {
     user: User;
+    singInWithGoogle(): Promise<void>;
 }
 
 const AuthContext = createContext({} as IAuthContextData);
@@ -24,8 +26,27 @@ function AuthProvider({children}: AuthProviderProps) {
         email: 'fulano@email.com',
     }
 
+    async function singInWithGoogle() {
+        try { 
+            const CLIENT_ID = '620871080151-reau79nl9bgfgu0bdpkdvs82b4m3k2he.apps.googleusercontent.com';
+            const REDIRECT_URI = 'https://auth.expo.io/@gabrielmeds/gofinances';
+            const RESPONSE_TYPE = 'token';
+            const SCOPE = encodeURI('profile email');
+
+            const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id${CLIENT_ID}&redirect_uri${REDIRECT_URI}&response_type${RESPONSE_TYPE}&scope${SCOPE}`;
+
+            const response = await AuthSession.startAsync({ authUrl });
+            console.log(response);
+
+        } catch (error) {
+            throw new Error(error);
+          }
+    }
+
     return(
-        <AuthContext.Provider value={{ user }}>
+        <AuthContext.Provider value={{ 
+            user,
+            singInWithGoogle }}>
             {children}
         </AuthContext.Provider>
     )
