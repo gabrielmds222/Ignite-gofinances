aefc8556404dbebf1a8d5976d4a91f07d11513d5
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-09-03 00:46:27 -0300

    rm: Remove comentario de teste

--- src/screens/dashboard/styles.ts
+++ src/screens/dashboard/styles.ts
@@ -1,98 +1,99 @@
-import styled from 'styled-components/native';
-import { Feather } from '@expo/vector-icons';
-import { FlatList, FlatListProps } from 'react-native';
-import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
-import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
- 
-import { DataListProps } from '.';
+import styled from "styled-components/native";
+import { Feather } from "@expo/vector-icons";
+import { FlatList, FlatListProps } from "react-native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import {
+  getStatusBarHeight,
+  getBottomSpace,
+} from "react-native-iphone-x-helper";
+
+import { DataListProps } from ".";
 
 export const Container = styled.View`
-    flex: 1;
-    background-color: ${({theme}) => theme.colors.background};
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Header = styled.View`
-    width: 100%;
-    height: ${RFPercentage(42)}px;
+  width: 100%;
+  height: ${RFPercentage(42)}px;
 
-    background-color: ${({theme}) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.primary};
 
-    justify-content: center;
-    align-items: flex-start;
-    flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: row;
 `;
 
 export const UserWrapper = styled.View`
-    width: 100%;
+  width: 100%;
 
-    padding: 0 24px;
-    margin-top: ${getStatusBarHeight() + RFValue(28)}px;
+  padding: 0 24px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
 
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const UserInfo = styled.View`
-    flex-direction: row;
-    align-items: center;
+  flex-direction: row;
+  align-items: center;
 `;
 
 export const Photo = styled.Image`
-    width: ${RFValue(48)}px;
-    height: ${RFValue(48)}px;
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
 
-    border-radius: 10px;
+  border-radius: 10px;
 `;
 
 export const User = styled.View`
-    margin-left: 17px;
+  margin-left: 17px;
 `;
 
 export const UserGreeting = styled.Text`
-    color: ${({theme}) => theme.colors.shape};
+  color: ${({ theme }) => theme.colors.shape};
 
-    font-size: ${RFValue(18)}px;
-    font-family: ${({theme}) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
 `;
 
 export const UserName = styled.Text`
-color: ${({theme}) => theme.colors.shape};
+  color: ${({ theme }) => theme.colors.shape};
 
-font-size: ${RFValue(18)}px;
-font-family: ${({theme}) => theme.fonts.bold};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
 `;
 
-// Teste de commit
-
 export const Icon = styled(Feather)`
-    color: ${({theme}) => theme.colors.secondary};
-    font-size: ${RFValue(24)}px;
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
 `;
 
 export const HighLightCards = styled.ScrollView.attrs({
-    horizontal: true, 
-    showsHorizontalScrollIndicator: false,
-    contentContainerStyle: {paddingHorizontal: 24}
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 },
 })`
-    width: 100%;
+  width: 100%;
 
-    position: absolute;
-    margin-top: ${RFPercentage(20)}px;
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
 `;
 
 export const Transactions = styled.View`
-    flex: 1%;
-    padding: 0 24px;
+  flex: 1%;
+  padding: 0 24px;
 
-    margin-top: ${RFPercentage(14)}px;
+  margin-top: ${RFPercentage(14)}px;
 `;
 
 export const Title = styled.Text`
-    font-size: ${RFValue(18)}px;
-    font-family: ${({theme}) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
 
-    margin-bottom: 16px;
+  margin-bottom: 16px;
 `;
 
 export const TransactionList = styled(
@@ -103,11 +104,11 @@ export const TransactionList = styled(
   showsVerticalScrollIndicator: false,
   contentContainerStyle: {
     paddingBottom: getBottomSpace(),
-  }
+  },
 })``;
 
 export const LoadContainer = styled.View`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 `;
